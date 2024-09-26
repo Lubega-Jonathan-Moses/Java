b784f9d010d80e4df25f7022078c9d49aa48e4b6
Lubega-Jonathan-Moses <jonbegas12@gmail.com>
2024-09-26 19:10:26 +0300

    Add files via upload

---
+++ ExerciseFour.java
@@ -0,0 +1,11 @@
+public class ExerciseFour {
+    public static int addNumbers(int a, int b) {
+        int sum = a + b;     
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int result = addNumbers(5,7);
+        System.out.println("Sum: " + result);
+    }
+}
